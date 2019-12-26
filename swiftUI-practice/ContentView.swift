5064bcaa18f871d17b0d2bc0fef04cfd75cd55c5
inyeongkim <hacerrumbo@gmail.com>
2019-12-26 18:51:44 +0900

    Initial Commit

---
+++ swiftUI-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-practice
+//
+//  Created by INYEONGKIM on 2019/12/26.
+//  Copyright © 2019 INYEONGKIM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
