812bdcc90bd5cc690128031a6ec66da4a2860257
inyeongkim <hacerrumbo@gmail.com>
2019-12-28 00:14:45 +0900

    add : Building Lists and Navigation

---
+++ swiftUI-practice/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  swiftUI-practice
+//
+//  Created by INYEONGKIM on 2019/12/27.
+//  Copyright © 2019 INYEONGKIM. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView{
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+    }
+}
